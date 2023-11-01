967f389fefe9797e21e76e7b77defc0131de1aa0
sansonejj <79421545+sansonejj@users.noreply.github.com>
2023-10-31 22:31:38 -0400

    Commit 10/31/2023

--- template.php
+++ template.php
@@ -1,30 +1,39 @@
+<!-- xhtml headings reference by https://www.w3docs.com/learn-html/xhtml-extensible-hypertext-markup-language.html-->
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en">
 <head>
-    <title>Sansone Liberty</title>
+    <title>Insert Title Here</title>
     <!-- The link below points to the CSS styles sheet where my 3 custom CSS classes are located -->
-    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" type="text/css" href="styles.css" />
+
 </head>
 <body>
 <div class="navigation">
     <?php
-    // include statement for php file will go here
+    //include //Put the referenced php file here for navigation and remove the // in front of 'include'
     ?>
-
 </div>
 
 <div class="page-header">
     <?php
-    echo "<h1>Page Header</h1>";
+    echo "<h1></h1>";
     ?>
 </div>
 
-<div class="paragraph">
+<div class="body">
     <?php
-    // Use echo to add a paragraph
-    echo "<p>Body stuff goes here</p>";
+    //This is where the body or text content will go
+    echo "<p></p>";
     ?>
 </div>
 
+<div class="custom-class1">
+    <?php
+    include 'pageModifiedDate.php';
+    ?>
+</div>
+
+<!--  -->
 </body>
 </html>
--- w1_phpInfo.php
+++ w1_phpInfo.php
@@ -1,1 +1,39 @@
-<?php
+<!-- xhtml headings reference by https://www.w3docs.com/learn-html/xhtml-extensible-hypertext-markup-language.html-->
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head>
+    <title>PHP Info</title>
+    <!-- The link below points to the CSS styles sheet where my 3 custom CSS classes are located -->
+    <link rel="stylesheet" type="text/css" href="styles.css" />
+
+</head>
+<body>
+<div class="navigation">
+    <?php
+    include 'week1Nav.php'
+    ?>
+</div>
+
+<div class="page-header">
+    <?php
+    echo "<h1>PHP Information</h1>";
+    ?>
+</div>
+
+<div class="paragraph">
+    <?php
+    //Reference: https://stackoverflow.com/questions/7190093/is-there-an-easy-way-of-seeing-php-info
+    print "\n" . (phpinfo());
+    ?>
+</div>
+
+<div class="custom-class1">
+    <?php
+   include 'pageModifiedDate.php';
+    ?>
+</div>
+
+<!--  -->
+</body>
+</html>
