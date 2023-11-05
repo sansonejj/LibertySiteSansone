912731512a78cca780034cfb6235b7c65439827a
sansonejj <79421545+sansonejj@users.noreply.github.com>
2023-11-05 14:08:10 -0500

    Commit 11/05/2023 13 52

--- w1_orgchart.php
+++ w1_orgchart.php
@@ -40,13 +40,13 @@
         'employee2' => array(
             'name' => $employee2_name,
             'link' => 'w1_emp1_TL.php',
-            'image' => 'emp_tl.jpeg',
+            'image' => 'emp_TL.jpeg',
             'job_title' => $employee2_job_title
         ),
         'employee3' => array(
             'name' => $employee3_name,
             'link' => 'w1_emp1_AL.php',
-            'image' => 'emp_al.jpg' ,
+            'image' => 'emp_AL.jpg' ,
             'job_title' => $employee3_job_title
 
         ),
@@ -56,7 +56,7 @@
     foreach ($employees as $employee) {
         //Reference: https://www.shecodes.io/athena/22112-how-to-display-a-list-horizontally-in-html-and-css#:~:text=To%20present%20a%20list%20horizontally%20instead%20of%20vertically%20in%20HTML,property%20on%20the%20list%20items.&text=This%20will%20display%20the%20bulleted,the%20value%20of%20margin-right%20
         //used on line 59
-        echo '<li style="display: inline-block; margin-right: 10px">';
+        echo '<li style="display: inline-block; margin-right: 40px">';
         echo '<img src="' . $employee['image'] . '" alt="' . $employee['name'] . '" width="200" height="200" />';
         echo '<br>';
         echo '<a href="' . $employee['link'] . '">' . $employee['name'] . '</a>';
